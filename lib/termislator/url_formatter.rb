4d9d662e1484c0d6ef82f32ff9f252bc4d8f3a62
pawurb <p.urbanek89@gmail.com>
2013-08-18 17:30:46 +0200

    added url formatter and specs

--- lib/termislator/url_formatter.rb
+++ lib/termislator/url_formatter.rb
@@ -1,5 +1,18 @@
 module Termislator
   class UrlFormatter
+    def initialize request_options
+      @request_options = request_options
+    end
 
+    def call
+      construct_url
+    end
+
+    private
+
+      def construct_url
+        data = @request_options
+        "http://translate.google.com/?tl=#{data[:target_lang]}&sl=#{data[:source_lang]}&ie=UTF-8&oe=UTF-8"
+      end
   end
 end
--- spec/url_formatter_spec.rb
+++ spec/url_formatter_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Termislator::UrlFormatter do
-  pending "UrlFormatter specs waitin"
+  describe "call method" do
+    context "when voice flag is set to false" do
+      it "returns correctly formatted url" do
+        url_formatter = Termislator::UrlFormatter.new  source_lang: :en, target_lang: :fr, text: 'your mother', voice: false
+        url = "http://translate.google.com/?tl=fr&sl=en&ie=UTF-8&oe=UTF-8"
+        expect(url_formatter.call).to eq(url)
+      end
+    end
+  end
+
 end
