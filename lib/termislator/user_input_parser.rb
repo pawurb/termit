53ced23f5b94c8946c47b1364e9e7297be733271
pawurb <p.urbanek89@gmail.com>
2013-08-18 17:21:19 +0200

    finished user_input_parser and specs

--- lib/termislator/user_input_parser.rb
+++ lib/termislator/user_input_parser.rb
@@ -1,5 +1,40 @@
 module Termislator
   class UserInputParser
 
+    def initialize user_input
+      raise ArgumentError unless user_input.is_a? Array
+      @user_input = user_input
+    end
+
+    def call
+      parsed = parse_input
+      validate_user_input parsed
+      parsed
+    rescue Exception => err
+      raise ArgumentError, "Wrong data: #{err.message}"
+    end
+
+    private
+
+      def parse_input
+        parsed = {
+         voice: set_voice_flag,
+         source_lang: @user_input.shift.to_sym,
+         target_lang: @user_input.shift.to_sym,
+         text: @user_input.join(' ')
+        }
+
+      end
+
+      def set_voice_flag
+        flag_index = @user_input.index('-v')
+        flag_index ? !!@user_input.delete_at(flag_index) : false
+      end
+
+      def validate_user_input hash
+        length_error = "must be two letter long string. Try: 'en', 'fr, 'pl' etc."
+        raise ArgumentError, "Target language #{length_error}" unless (hash[:source_lang].length == 2)
+        raise ArgumentError, "Target language #{length_error}" unless (hash[:target_lang].length == 2)
+      end
   end
 end
