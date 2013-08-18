3d325279a640a955a464b15e302d09b4fae81295
pawurb <p.urbanek89@gmail.com>
2013-08-18 18:32:40 +0200

    added data_fetcher and specs

--- lib/termislator/data_fetcher.rb
+++ lib/termislator/data_fetcher.rb
@@ -1,5 +1,25 @@
+require 'net/http'
+require 'uri'
+
 module Termislator
   class DataFetcher
+    def initialize url, text
+      @url = url
+      @text = text
+    end
 
+    def call
+      send_request
+    end
+
+    private
+      def send_request
+        uri = URI.parse @url
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.request_uri)
+        request.set_form_data(text: @text)
+        response = http.request(request)
+        response
+      end
   end
 end
--- spec/data_fetcher_spec.rb
+++ spec/data_fetcher_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
+
 describe Termislator::DataFetcher do
-  pending "DataFetcher specs waitin"
+  describe "call method" do
+    let(:url) { "http://translate.google.com/?tl=fr&sl=en&ie=UTF-8&oe=UTF-8" }
+    let(:text) { "it would require two large integer variables to store her weight, else she would cause a buffer overflow." }
+    it "should send the correct POST request" do
+      fetcher = Termislator::DataFetcher.new url, text
+      Net::HTTP.should_receive(:new)
+      request = double 'fake_request'
+      http = double 'fake_http', request: :good_response
+      Net::HTTP::Post.stub(:new) { request }
+      Net::HTTP::stub(:new) { http }
+      http.should_receive(:request).with(request)
+      request.should_receive(:set_form_data).with(text: text)
+      fetcher.call.should eq :good_response
+    end
+  end
 end
