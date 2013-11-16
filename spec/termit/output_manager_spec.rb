321f17f526efb394633d8f8a636a832fcb13bf86
pawurb <p.urbanek89@gmail.com>
2013-11-16 17:32:15 +0100

    use symbols not strings

--- spec/termit/output_manager_spec.rb
+++ spec/termit/output_manager_spec.rb
@@ -2,13 +2,14 @@
 require 'spec_helper'
 
 describe Termit::OutputManager do
-  METHOD_NAMES = ["display_error_info_and_quit", "display_help_and_quit",
-                  "display_version_and_quit", "display_player_error_and_quit"]
+  METHOD_NAMES = [:display_error_info_and_quit, :display_help_and_quit, :display_version_and_quit, :display_player_error_and_quit]
   subject { Termit::OutputManager.new }
 
   describe "instance_methods" do
     it "does what it says" do
-      METHOD_NAMES.each { |m| expect { subject.send(m) }.to raise_error SystemExit }
+      METHOD_NAMES.each do |m|
+        expect { subject.send(m) }.to raise_error SystemExit
+      end
     end
   end
 end
